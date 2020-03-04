39c303570600fb435673979c1fc8ebfe9503cafa
ECCC-CSSP <charles.leblanc2@canada.ca>
2020-03-04 16:41:40 -0400

    Init

--- WQMS.RTNotifiers/ASAS_old2.cs
+++ WQMS.RTNotifiers/ASAS_old2.cs
@@ -38,10 +38,8 @@ namespace WQMS.RTNotifiers
 
             Boolean bAnyProblemsFound = false;
 
-            //strMessage parts   Feb 28, 2020. Modify so that summary can include a bit of station-specific detail. Create strMessage in parts
+            //strMessage
             string strMessage = "";
-            string strMessageHeader = "";
-            string strMessageBody = "";
 
             //limit values
             double rawDataValue = 0;
@@ -49,19 +47,19 @@ namespace WQMS.RTNotifiers
             double lowerLimit = 0;
             string varUnit = "";
 
-            strMessageHeader = "Issues found at Automated WQ Stations \r\n\r\n";
-            strMessageHeader += "Total of " + dtNotifyStations.Rows.Count.ToString() + " stations are flagged for Notification: \r\n";
-            strMessageHeader += "Checking back: " + hoursToCheckBack + " hours from current time\r\n";
-            strMessageHeader += "----------------------\r\n";
-
-            //Feb 28, 2020. Comment out. This part of message will be built within station loop below.
-            //foreach (DataRow dr in dtNotifyStations.Rows)
-            //{
-            //    strMessage += dr["stationEnvdatID"].ToString() + "\r\n";
-            //}
+            strMessage = "Issues found at Automated WQ Stations \r\n\r\n";
+            strMessage += "Total of " + dtNotifyStations.Rows.Count.ToString() + " stations are flagged for Notification: \r\n";
+            strMessage += "Checking back: " + hoursToCheckBack + " hours from current time\r\n";
+            strMessage += "----------------------\r\n";
 
 
-            strMessageBody += "----------------------\r\n\r\n";
+            foreach (DataRow dr in dtNotifyStations.Rows)
+            {
+                strMessage += dr["stationEnvdatID"].ToString() + "\r\n";
+            }
+
+
+            strMessage += "----------------------\r\n\r\n";
 
             //strMessage += "============STATION LISTING==================\r\n\r\n"; 
 
@@ -76,33 +74,25 @@ namespace WQMS.RTNotifiers
                 double dbsas = double.Parse(drStatz["UTCOffset"].ToString());
 
 
-                //Add to header part of message
-                strMessageHeader += drStatz["stationEnvdatID"].ToString() + "\t" + drStatz["stationName"].ToString().PadRight(72) + "\t";
-
-                //Details part of message
                 //Output Station Name and Time Zone. 
-                strMessageBody += "========" + drStatz["stationEnvdatID"].ToString() + "  -  " + drStatz["stationName"].ToString() + "============\r\n\r\n";
+                strMessage += "========" + drStatz["stationEnvdatID"].ToString() + "  -  " + drStatz["stationName"].ToString() + "============\r\n\r\n";
                 //strMessage += "----------------------\r\n\r\n";
-                strMessageBody += "Time Period for Analysis (" + drStatz["timeZoneCode"].ToString() + ")\r\n";
-                strMessageBody += "----------------------------------------------\r\n";
-                strMessageBody += "Start: " + startDate.AddHours(double.Parse(drStatz["UTCOffset"].ToString())).ToString("yyyy-MM-dd HH:mm") + "\r\n";
-                strMessageBody += "End:  " + endDate.AddHours(double.Parse(drStatz["UTCOffset"].ToString())).ToString("yyyy-MM-dd HH:mm") + "\r\n";
-                strMessageBody += "Total: " + hoursToCheckBack + " hours\r\n";
-                strMessageBody += "----------------------------------------------\r\n\r\n";
-                strMessageBody += "Problem Details:\r\n";
+                strMessage += "Time Period for Analysis (" + drStatz["timeZoneCode"].ToString() + ")\r\n";
+                strMessage += "----------------------------------------------\r\n";
+                strMessage += "Start: " + startDate.AddHours(double.Parse(drStatz["UTCOffset"].ToString())).ToString("yyyy-MM-dd HH:mm") + "\r\n";
+                strMessage += "End:  " + endDate.AddHours(double.Parse(drStatz["UTCOffset"].ToString())).ToString("yyyy-MM-dd HH:mm") + "\r\n";
+                strMessage += "Total: " + hoursToCheckBack + " hours\r\n";
+                strMessage += "----------------------------------------------\r\n\r\n";
+                strMessage += "Problem Details:\r\n";
 
 
 
                 //look for rows of data for this station
                 if (dtStationNotifyData.Select("stationID = '" + drStatz["stationID"].ToString() + "'").Length == 0)
                 {
-
-                    //No rows. Add to summary.
-                    strMessageHeader += "No data transferred to database for past " + hoursToCheckBack.ToString() + " hours.\r\n";
-
-                    //No Rows. Append Error Message to details. 
-                    strMessageBody += "No data transferred to database for " + drStatz["stationEnvdatID"] + " for past " + hoursToCheckBack.ToString() + " hours. Try connecting directly to logger to troubleshoot and ensure station is down\r\n";
-                    strMessageBody += "If station " + drStatz["stationEnvdatID"] + " is not active or notifications are not necessary, ask administrator to set notify flag to off\r\n";
+                    //No Rows. Append Error Message. 
+                    strMessage += "No data transferred to database for " + drStatz["stationEnvdatID"] + " for past " + hoursToCheckBack.ToString() + " hours. Try connecting directly to logger to troubleshoot and ensure station is down\r\n";
+                    strMessage += "If station " + drStatz["stationEnvdatID"] + " is not active or notifications are not necessary, ask administrator to set notify flag to off\r\n";
                     bProblemFound = true;
                 }
                 else
@@ -125,12 +115,12 @@ namespace WQMS.RTNotifiers
                             varUnit = foundDataForStation[i]["unitValue"].ToString();
                             if (rawDataValue < lowerLimit)
                             {
-                                strMessageBody += foundDataForStation[i]["variableName"].ToString() + " value: " + rawDataValue.ToString() + " " + varUnit + " at station: " + foundDataForStation[i]["stationEnvdatID"].ToString() + " at " + foundDataForStation[i]["dataMeasurementDate"] + " is below lower limit of " + lowerLimit.ToString() + "\r\n";
+                                strMessage += foundDataForStation[i]["variableName"].ToString() + " value: " + rawDataValue.ToString() + " " + varUnit + " at station: " + foundDataForStation[i]["stationEnvdatID"].ToString() + " at " + foundDataForStation[i]["dataMeasurementDate"] + " is below lower limit of " + lowerLimit.ToString() + "\r\n";
                                 bProblemFound = true;
                             }
                             if (rawDataValue > upperLimit)
                             {
-                                strMessageBody += foundDataForStation[i]["variableName"].ToString() + " value: " + rawDataValue.ToString() + " " + varUnit + " at station: " + foundDataForStation[i]["stationEnvdatID"].ToString() + " at " + foundDataForStation[i]["dataMeasurementDate"] + " is greater than upper limit of " + upperLimit.ToString() + "\r\n";
+                                strMessage += foundDataForStation[i]["variableName"].ToString() + " value: " + rawDataValue.ToString() + " " + varUnit + " at station: " + foundDataForStation[i]["stationEnvdatID"].ToString() + " at " + foundDataForStation[i]["dataMeasurementDate"] + " is greater than upper limit of " + upperLimit.ToString() + "\r\n";
                                 bProblemFound = true;
                             }
                         }
@@ -138,12 +128,7 @@ namespace WQMS.RTNotifiers
 
                     if (bProblemFound == false)
                     {
-                        strMessageHeader += "No problems found for this station\r\n";
-                        strMessageBody += "No problems found for this station\r\n";
-                    }
-                    else
-                    {
-                        strMessageHeader += "Problems found for this station. See details below.\r\n";
+                        strMessage += "No problems found for this station\r\n";
                     }
                 }
 
@@ -153,7 +138,7 @@ namespace WQMS.RTNotifiers
 
 
                 //add spaces before moving to next station
-                strMessageBody += "\r\n\r\n";
+                strMessage += "\r\n\r\n";
 
                 //if a problem was found with this station, set the overall problem found flag to true and add 
                 //the station to a list of station that had problems.
@@ -166,12 +151,12 @@ namespace WQMS.RTNotifiers
 
             }
 
-            strMessageBody += "=============END SUMMARY===================\r\n\r\n";
+            strMessage += "=============END SUMMARY===================\r\n\r\n";
 
-            strMessageBody += "*************************************************************************************************************************************************\r\n";
-            strMessageBody += "You have received this message because you are assigned as a contact for Automated Water Quality Station(s) that is/are experiencing problems.\r\n";
-            strMessageBody += "If you are receiving this in error or no longer want notifications, reply to this email with details.\r\n";
-            strMessageBody += "**************************************************************************************************************************************************\r\n\r\n";
+            strMessage += "*************************************************************************************************************************************************\r\n";
+            strMessage += "You have recieved this message because you are assigned as a contact for Automated Water Quality Station(s) that is/are experiencing problems.\r\n";
+            strMessage += "If you are recieving this in error or no longer want notifications, reply to this email with details.\r\n";
+            strMessage += "**************************************************************************************************************************************************\r\n\r\n";
 
 
             //********IF PROBLEM FOUND - SEND EMAIL**********
@@ -191,13 +176,11 @@ namespace WQMS.RTNotifiers
                     }//Do nothing if these eamils are found --quick way to not notify them instead of changing the tables in database :20 Mar 2018 S. Donohue
                     else
                     {
-                        strMessageBody += row["techEmail"].ToString() + ",";
+                        strMessage += row["techEmail"].ToString();
                     }
                 }
                 //strMessage += "shawn.donohue@canada.ca"; //also add my name here to get tagged on. Shawn Donohue 20 mar 2018
-                strMessageBody += "David.Halliwell@canada.ca"; //Shawn Donohue 7 mar 2019 removed his name above and added David Halliwell
-
-                strMessage += strMessageHeader + strMessageBody;
+                strMessage += "David.Halliwell@canada.ca"; //Shawn Donohue 7 mar 2019 removed his name above and added David Halliwell
                 SendEmail(strMessage, dtContacts);
 
             }
